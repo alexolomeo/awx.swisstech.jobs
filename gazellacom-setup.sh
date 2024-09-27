aa8e6a878994a0a6933b2979ad21b8c6dbe3ace0
alexolomeo <alexolomeo@gmail.com>
2024-09-27 15:07:32 +0000

    FIX JOB 4

--- gazellacom-setup.sh
+++ gazellacom-setup.sh
@@ -5,16 +5,40 @@
 #
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-
+RETVAL=0
 BACKUPDATE="$(date +'%Y%m%d-%H%M')"
 
 ##
 ## Prepare repository
 ##
 
+\rm -rf roles key awx.swisstech.playbook
+
 git clone git@github.com:alexolomeo/awx.swisstech.playbook.git
 
 ln -s awx.swisstech.playbook/roles roles
 
-ansible-playbook -i inventory/setup.gomax.integration gazellacom.config.integration.yml
+usage ()
+{
+	echo $"Usage: $0 {integration|int|production|prod}" 1>&2
+	RETVAL=2
+}
 
+integration ()
+{
+	ansible-playbook -i inventory/setup.integration gazellacom.config.integration.yml
+}
+
+production ()
+{
+	ansible-playbook -i inventory/setup.production gazellacom.config.production.yml
+}
+
+
+case "$1" in
+    integration|int) integration ;;
+    production|prod) production ;;
+    *) usage ;;
+esac
+
+exit $RETVAL
